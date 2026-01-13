a6be0c2807970b00f776488724aaab6f45b71955
Starboy7704 <saipraneethmokalla@gmail.com>
2026-01-14 03:30:04 +0530

    feat: enhance ManageProducts component with seed form submission; update Appwrite integration and constants

--- src/Appwrite/TableDB.services.js
+++ src/Appwrite/TableDB.services.js
@@ -1,56 +1,26 @@
-import { TablesDB, ID } from "appwrite"
-import appwriteClient from "."
-import { APPWRITE_DB_ID } from "../Utils/Appwrite/constants.js";
+import { TablesDB, ID } from "appwrite";
+import appwriteClient from ".";
+import { APPWRITE_DATABASE_ID } from "../Utils/Appwrite/constants.js";
 
 class AppwriteTablesDB {
-    constructor() {
-        this.tablesDb = new TablesDB(appwriteClient)
+  constructor() {
+    this.tablesDB = new TablesDB(appwriteClient);
+  }
+
+  async createRow(tablesId, data) {
+
+    try {
+      const result = await this.tablesDB.createRow({
+        databaseId: APPWRITE_DATABASE_ID,
+        tableId: tablesId,
+        rowId: ID.unique(),
+        data: data 
+      });
+      return result;
+    } catch (error) {
+        console.error(error);
+        throw new Error(error.message)
     }
-
-    async createRow(tableId, data) {
-        try {
-            const result = await this.tablesDb.createRow({
-                databaseId: APPWRITE_DB_ID,
-                tableId: tableId,
-                rowId: ID.unique(),
-                data: data
-            });
-
-            return result
-        } catch (error) {
-            console.error(error)
-            throw new Error(error.message)
-        }
-    }
-
-    async listRows(tableId) {
-        try {
-            const result = await this.tablesDb.listRows({
-                databaseId: APPWRITE_DB_ID,
-                tableId: tableId
-            });
-
-            return result.rows;
-        } catch (error) {
-            console.error(error)
-            throw new Error(error.message)
-        }
-    }
-
-    async getRow(tableId, rowId){
-        try {
-            const result = await this.tablesDb.getRow({
-                databaseId: APPWRITE_DB_ID,
-                tableId: tableId,
-                rowId: rowId
-            });
-
-            return result;
-        } catch (error) {
-            console.error(error)
-            throw new Error(error.message)
-        }
-    }
+  }
 }
-
 export default AppwriteTablesDB;
